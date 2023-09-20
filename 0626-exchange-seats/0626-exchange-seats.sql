c67502e650fb39a91e996c1e23e12f05f7017e0d
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 23:56:20 +0200

    Time: 671 ms (61.55%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,17 +1,9 @@
 /* Write your T-SQL query statement below */
-WITH CTE AS (
 SELECT 
 CASE 
-    WHEN id %2 = 0 THEN LAG(id,1) OVER(ORDER BY id ASC)
-    WHEN id %2 <> 0 THEN LEAD(id,1) OVER(ORDER BY id ASC) 
+    WHEN id %2 = 0 THEN ISNULL(LAG(id,1) OVER(ORDER BY id ASC),id)
+    WHEN id %2 <> 0 THEN ISNULL(LEAD(id,1) OVER(ORDER BY id ASC),id)
     END AS id
 , student
 FROM Seat
-)
-SELECT 
-CASE WHEN CTE.id IS NULL THEN s.id ELSE CTE.id END AS id
-, CTE.student
-FROM CTE
-JOIN Seat s 
-    on CTE.student = s.student
 ORDER BY 1
