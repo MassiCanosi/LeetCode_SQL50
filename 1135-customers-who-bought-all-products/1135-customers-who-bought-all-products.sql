520f7595c5e415d407c16c4fde7ac03ddad1ac20
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 21:35:40 +0200

    Time: 2900 ms (62.78%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,22 +1,11 @@
 /* Write your T-SQL query statement below */
-WITH CTE AS (
-SELECT DISTINCT
+SELECT 
 c.customer_id
-, COUNT(DISTINCT c.product_key) AS product_per_customer
-FROM Customer c
-JOIN Product p 
-  ON c.product_key = p.product_key
+FROM Customer c 
 GROUP BY 
 c.customer_id
-)
-SELECT 
-CTE.customer_id
-FROM CTE 
-JOIN (
-  SELECT 
-  COUNT(DISTINCT product_key) as total
-  FROM Product
-) a on CTE.product_per_customer = a.total 
+HAVING COUNT(DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product)
+
 
 
 
