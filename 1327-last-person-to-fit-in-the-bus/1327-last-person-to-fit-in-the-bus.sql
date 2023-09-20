4881ad6d90e7b6e570084b772e1a62d2fa163398
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 22:39:18 +0200

    Time: 2821 ms (43.09%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,27 +1,9 @@
 /* Write your T-SQL query statement below */
-WITH CTE AS (
-  SELECT * FROM (
-    SELECT 
-    person_id
-    , person_name
-    , weight
-    , turn
-    , SUM(weight) OVER(ORDER BY turn ASC) otp
-    FROM Queue
-    GROUP BY 
-    person_id
-    , person_name
-    , turn
-    , weight
-  ) v
-  WHERE v.otp <= 1000
-), CTE1 AS (
+SELECT TOP 1 person_name FROM (
   SELECT 
-  CTE.*
-  , ROW_NUMBER() OVER (ORDER BY CTE.turn DESC) RN
-  FROM CTE
-)
-SELECT 
-CTE1.person_name
-FROM CTE1
-WHERE CTE1.RN=1 
+  Queue.*
+  , SUM(weight) OVER(ORDER BY turn) as run_tot
+  FROM Queue 
+) a
+WHERE a.run_tot <= 1000
+ORDER BY a.turn DESC
