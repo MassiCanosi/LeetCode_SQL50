ef625b041cb0f8d05b7f260a1a7067a09aa16942
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 23:59:40 +0200

    Time: 679 ms (59.86%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 SELECT 
 CASE 
-    WHEN id %2 = 0 THEN ISNULL(LAG(id,1) OVER(ORDER BY id ASC),id)
+    WHEN id %2 = 0 THEN LAG(id,1) OVER(ORDER BY id ASC)
     WHEN id %2 <> 0 THEN ISNULL(LEAD(id,1) OVER(ORDER BY id ASC),id)
     END AS id
 , student
