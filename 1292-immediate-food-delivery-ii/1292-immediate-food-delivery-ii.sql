a206775ff48e9da7388cad8b023881e0e14f1bf6
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 20:02:52 +0200

    Time: 2179 ms (45.72%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -4,15 +4,10 @@ SELECT
 Delivery.*
 , ROW_NUMBER () OVER(PARTITION BY customer_id ORDER BY order_date ASC) RN
 FROM Delivery
-), CNT AS (
-SELECT 
-COUNT(DISTINCT CASE WHEN order_date = customer_pref_delivery_date 
-    THEN delivery_id
-    END) as immediate
-, count(*) as cnt 
-FROM RANKED r
-WHERE r.rn = 1
 )
 SELECT 
-ROUND(CAST(c.immediate AS DECIMAL)/CAST(c.cnt AS DECIMAL)*100,2) as immediate_percentage
-FROM CNT c
+ROUND(CAST(COUNT(DISTINCT CASE WHEN order_date = customer_pref_delivery_date 
+    THEN delivery_id
+    END) AS DECIMAL)/CAST(count(*) AS DECIMAL)*100,2) as immediate_percentage
+FROM RANKED r
+WHERE r.rn = 1
