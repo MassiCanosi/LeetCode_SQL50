f3f4f0874a20eb84ffaff89d14bd1ca573d322f2
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 22:06:50 +0200

    Time: 2126 ms (54.09%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,17 +1,18 @@
 /* Write your T-SQL query statement below */
 WITH CTE AS (
-SELECT 
-product_id
-, new_price
-, ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC) RN
-FROM Products
-WHERE change_date < '2019-08-17'
-), CTE1 AS (
-SELECT * FROM CTE WHERE CTE.RN=1
+  SELECT * FROM(
+    SELECT 
+    product_id
+    , new_price
+    , ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC) RN
+    FROM Products
+    WHERE change_date < '2019-08-17'
+    ) a
+  WHERE a.RN=1
 )
 SELECT DISTINCT
 p.product_id
-, ISNULL(CTE1.new_price,10) AS price
+, ISNULL(CTE.new_price,10) AS price
 FROM Products p
-LEFT JOIN CTE1
-  ON p.product_id = CTE1.product_id
+LEFT JOIN CTE
+  ON p.product_id = CTE.product_id
