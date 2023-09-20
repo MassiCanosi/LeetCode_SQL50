df33d6c557cd3a6f65bdbc5326000c94f3385242
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2023-09-20 20:32:57 +0200

    Time: 2824 ms (69.57%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -2,12 +2,12 @@
 WITH CTE AS(
   SELECT
   player_id
-  , event_date as first 
+  , event_date as date 
   , LEAD(event_date, 1) OVER (PARTITION BY player_id ORDER BY event_date ASC) as second
   , DENSE_RANK() OVER(PARTITION BY player_id ORDER BY event_date ASC) AS rk
   FROM Activity
 )
 SELECT 
-ROUND(CAST(COUNT(DISTINCT CASE WHEN DATEDIFF(DAY, CTE.first,CTE.second) = 1 THEN CTE.player_id END) AS DECIMAL) / CAST(COUNT(DISTINCT CTE.player_id) AS DECIMAL),2) as fraction
+ROUND(CAST(COUNT(DISTINCT CASE WHEN DATEDIFF(DAY, CTE.date,CTE.second) = 1 THEN CTE.player_id END) AS DECIMAL) / CAST(COUNT(DISTINCT CTE.player_id) AS DECIMAL),2) as fraction
 FROM CTE
 WHERE CTE.rk = 1
